d5fae5729d0c50f4bc3f23137b19d0a0d75a8623
Cristiano Napoleao <cjnapoleao.dev@gmail.com>
2026-03-07 23:22:08 +0100

    feat: add complete/done feature

--- task.sh
+++ task.sh
@@ -6,28 +6,50 @@
 
 # First we will start with the adding tasks functionality.
 
-TASK_FILE_PATH="/Users/cjnapoleao.dev/dev/phase-0/tasks.txt"
+TASK_FILE_PATH="./tasks.txt"
+TOTAL_LINES=$( cat $TASK_FILE_PATH | wc -l )
+
 
 add () {
- local total_lines=$( cat $TASK_FILE_PATH | wc -l )
- local task_number=$(( total_lines + 1 ))
+ local task_number=$(( TOTAL_LINES + 1 ))
  
- echo $total_lines
- echo $task_number
- 
- if [ $total_lines -le 1  ]  
+ if [ $TOTAL_LINES -le 1  ]  
  
  then
-   printf "$task_number. $1" >> $TASK_FILE_PATH
-   printf "Your task has been inserted successfully ✅"
+   echo "$task_number. $1" >> $TASK_FILE_PATH
+   echo -n "Your task has been inserted successfully ✅"
+ 
  else
-   printf "\n$task_number. $1" >> $TASK_FILE_PATH
-   printf "Your task has been inserted successfully ✅"
+   echo -ne "\n$task_number. $1" >> $TASK_FILE_PATH
+   echo -n "Your task has been inserted successfully ✅"
  fi
  return 0
 }
 
+completed () {
+  
+  if [ $TOTAL_LINES -lt 1  ]
+  then
+
+    echo -n "There are no tasks to be checked as completed." 
+  
+  elif [ $TOTAL_LINES -lt $1 ]
+  then
+
+    echo -n "There is no task $1."
+
+  else
+
+    local task=$( sed -n "$1p" $TASK_FILE_PATH )
+    sed -i "" "$1s/.*/$task ✅/" $TASK_FILE_PATH
+    echo -n "Your task has been updated successfully ✅"
+  
+  fi
+  return 0
+}
+
 if declare -f "$1" > /dev/null; then
+
   function_name=$1
   shift 
   "$function_name" "$@"
