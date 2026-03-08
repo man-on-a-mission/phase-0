a5411a1833e4018a1708a52ce682e9097daa01f1
Cristiano Napoleao <cjnapoleao.dev@gmail.com>
2026-03-08 19:37:31 +0100

    create list feature
    refactor code logic

--- task.sh
+++ task.sh
@@ -2,49 +2,56 @@
 
 # This script will add a task in text format to a file. 
 # Each new task should be numbered automatically.
-# Here we will be able to add, delete, and set tasks as done.
+# Here we will be able to add, delete, list, and set tasks as done.
 
 # First we will start with the adding tasks functionality.
 
 TASK_FILE_PATH="./tasks.txt"
 TOTAL_LINES=$( cat $TASK_FILE_PATH | wc -l )
 
-
-add () {
- local task_number=$(( TOTAL_LINES + 1 ))
- 
- if [ $TOTAL_LINES -le 1  ]  
- 
- then
-   echo "$task_number. $1" >> $TASK_FILE_PATH
-   echo -n "Your task has been inserted successfully ✅"
- 
- else
-   echo -ne "\n$task_number. $1" >> $TASK_FILE_PATH
-   echo -n "Your task has been inserted successfully ✅"
- fi
- return 0
-}
-
-completed () {
-  
+has_tasks () {
   if [ $TOTAL_LINES -lt 1  ]
   then
-
-    echo -n "There are no tasks to be checked as completed." 
-  
-  elif [ $TOTAL_LINES -lt $1 ]
-  then
-
-    echo -n "There is no task $1."
-
+    echo -n "You must create a task first. Try sh task.sh add <task_description>"
+    return 1
   else
-
-    local task=$( sed -n "$1p" $TASK_FILE_PATH )
-    sed -i "" "$1s/.*/$task ✅/" $TASK_FILE_PATH
-    echo -n "Your task has been updated successfully ✅"
-  
+    return 0
   fi
+}
+
+
+
+add () {
+
+  local task_number=$(( TOTAL_LINES + 1 ))
+ 
+  echo "$task_number. $1" >> $TASK_FILE_PATH
+  echo -n "Your task has been inserted successfully ✅"
+  
+  return 0
+}
+
+complete () {
+  
+  if ! has_tasks; then
+    return 1
+  fi
+
+  local task=$( sed -n "$1p" $TASK_FILE_PATH )
+  sed -i "" "$1s/.*/$task ✅/" $TASK_FILE_PATH
+  echo -n "Your task has been updated successfully ✅"
+  
+  return 0
+  
+}
+
+list () {
+
+  if ! has_tasks; then
+    return 1
+  fi
+  
+  cat $TASK_FILE_PATH
   return 0
 }
 
